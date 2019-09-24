c5617c1e25007bc7aed354950d6ddd8b4769ae06
youjisang <jisangman89@gmail.com>
2019-09-24 13:50:49 +0900

    완주하지 못한 선수

--- programmers/src/marathon.java
+++ programmers/src/marathon.java
@@ -1,13 +1,67 @@
-import javax.sound.midi.SysexMessage;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 
 class Solution {
+
+    /**
+     * @param participant
+     * @param completion
+     * @return 최초  arrayList를 통한 풀이
+     * 그러나 시간이 오래걸려서 탈락.
+     */
+    //    public String solution(String[] participant, String[] completion) {
+//        String answer = "";
+//
+//        ArrayList<String> arrayList = new ArrayList<>();
+//
+//        for (int i = 0; i <= participant.length - 1; i++) {
+//            arrayList.add(participant[i]);
+//        }
+//        for (int x = 0; x <= completion.length - 1; x++) {
+//            arrayList.remove(completion[x]);
+//        }
+//
+//        for (int y = 0; y <= arrayList.size() - 1; y++) {
+//            System.out.print(arrayList.get(y));
+//            answer = arrayList.get(y);
+//        }
+//
+//        return answer;
+//    }
+
+    /**
+     * @param participant
+     * @param completion
+     * @return map을 통한 풀이
+     * 그러나 시간이 오래걸려서 탈락.
+     */
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+
+        Map<String, Integer> map = new HashMap<>();
+        int participantLength = participant.length;
+        int completionLength = completion.length;
+
+        for (int i = 0; i <= participantLength - 1; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        }
+        for (int x = 0; x <= completionLength - 1; x++) {
+            map.put(completion[x], map.getOrDefault(completion[x], 0) - 1);
+        }
+
+        for (String key : map.keySet()) {
+//            System.out.println(String.format("키 : %s, 값 : %s", key, map.get(key)));
+            if (map.get(key) == 1) {
+                answer = key;
+                break;
+            }
+        }
+
         return answer;
     }
-
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
@@ -16,12 +70,10 @@ class Solution {
         String participant_str = sc.nextLine();
         String completion_str = sc.nextLine();
         String[] participant = participant_str.substring(1, participant_str.indexOf("]")).replaceAll(" ", "").split(",");
-        for (int i = 0; i <= participant.length - 1; i++) {
-            System.out.print(participant[i]);
-        }
+        String[] completion = completion_str.substring(1, completion_str.indexOf("]")).replaceAll(" ", "").split(",");
 
-//        Solution sol = new Solution();
-//        System.out.print(sol.solution(participant, completion));
+        Solution sol = new Solution();
+        System.out.print(sol.solution(participant, completion));
 
     }
 
