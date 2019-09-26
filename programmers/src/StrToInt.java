bf9b49e8d469c0b4f4bca661a9c269f36251ff7a
youjisang <jisangman89@gmail.com>
2019-09-26 22:42:32 +0900

    문자를 숫자로. 내장 api말고 직접 생각해보기....

---
+++ programmers/src/StrToInt.java
@@ -0,0 +1,18 @@
+public class StrToInt {
+    public int solution(String s) {
+
+        int answer = Integer.parseInt(s);
+//        System.out.println(answer);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+
+        String qwer = "-1234";
+        StrToInt sol = new StrToInt();
+        sol.solution(qwer);
+
+    }
+}
