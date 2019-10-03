80ad69860183aea954ab7322a47a1eba6866e887
youjisang <jisangman89@gmail.com>
2019-10-03 16:21:13 +0900

    DeleteSmallNum

--- programmers/src/DeleteSmallNum.java
+++ programmers/src/DeleteSmallNum.java
@@ -1,26 +1,30 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class DeleteSmallNum {
 
     public int[] solution(int[] arr) {
-        int[] answer = new int[arr.length - 1];
+        int[] answer = {};
+        ArrayList list = new ArrayList();
+        answer = new int[arr.length - 1];
+        Arrays.sort(arr);
         int min = arr[0];
 
-        if (arr.length == 1) {
+        if (arr.length <= 1) {
             return new int[]{-1};
         }
 
-//        for (int i = 0; i < arr.length; i++) {
-//            if (min > arr[i]) {
-//                min = arr[i];
-//            }
-//        }
-
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > min) {
-                answer[i] = arr[i];
-                System.out.println(answer[i]);
-            }
+            list.add(arr[i]);
+        }
+
+        list.remove(list.indexOf(min));
+//        list.remove(min);
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = (int) list.get(i);
         }
 
         return answer;
@@ -30,7 +34,9 @@ public class DeleteSmallNum {
         // TODO Auto-generated method stub
 
 
-        int[] intArr = new int[]{5,4, 3, 2, 1,0,-5};
+        int[] intArr = new int[]{5, 4, 3, 2, 1, 0};
+//        int[] intArr = new int[]{1, 1, 1, 1};
+//        int[] intArr = new int[]{1};
         DeleteSmallNum sol = new DeleteSmallNum();
         sol.solution(intArr);
 
