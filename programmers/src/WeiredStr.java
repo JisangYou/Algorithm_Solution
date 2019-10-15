2412a20b11c72015235f0e272327d2a32c35a185
youjisang <jisangman89@gmail.com>
2019-10-16 00:53:20 +0900

    WeiredStr

--- programmers/src/WeiredStr.java
+++ programmers/src/WeiredStr.java
@@ -1,30 +1,33 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+
 
 public class WeiredStr {
     public String solution(String s) {
-        String answer = "";
-
-        String[] s_arr = s.split(" ");
-        ArrayList arrayList;
+        String[] s_arr = s.split("");
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
 
         for (int i = 0; i < s_arr.length; i++) {
-//            System.out.println(s_arr[i].toCharArray());
-            char[] chars = s_arr[i].toCharArray();
-            for (int x = 0; x < chars.length; x++) {
-                System.out.println(chars[x]);
+            if (!s_arr[i].equals(" ")) {
+                if (count == 0 || count % 2 == 0) {
+                    sb.append(s_arr[i].toUpperCase());
+                    count++;
+                } else {
+                    sb.append(s_arr[i].toLowerCase());
+                    count++;
+                }
+            } else {
+                sb.append(s_arr[i]);
+                count = 0;
             }
         }
-
-
-        return answer;
+        return sb.toString();
     }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
 
-        String s = "try hello world";
+        String s = " try hello world ";
         WeiredStr sol = new WeiredStr();
         sol.solution(s);
 
