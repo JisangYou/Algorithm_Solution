b5dfdef5929901bad66364278afe740f82b27856
youjisang <jisangman89@gmail.com>
2019-11-05 23:58:38 +0900

    MatrixSum ~ing

---
+++ programmers/src/MatrixSum.java
@@ -0,0 +1,17 @@
+public class MatrixSum {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+
+        int[][] arr1 = new int[][]{{1, 2}, {2, 3}};
+        int[][] arr2 = new int[][]{{3, 4}, {5, 6}};
+        MatrixSum sol = new MatrixSum();
+        sol.solution(arr1, arr2);
+
+    }
+}
