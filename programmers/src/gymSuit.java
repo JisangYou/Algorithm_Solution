2afffbae99a51b56e7ecd3f55456df24fe9ab1f5
youjisang <jisangman89@gmail.com>
2019-11-12 00:07:32 +0900

    gymSuit, greedy 문제 ~ing

--- programmers/src/gymSuit.java
+++ programmers/src/gymSuit.java
@@ -4,22 +4,28 @@ public class gymSuit {
     public int solution(int n, int[] lost, int[] reserve) {
 
         int answer = n - lost.length;
-
-        for (int i = 0; i < lost.length; i++) {
-            if (reserve[i] < lost[i]) {
-                answer++;
+        if (lost.length < reserve.length) {
+            for (int i = 0; i < lost.length; i++) {
+                if (reserve[i] < lost[i]) {
+                    answer++;
+                }
+            }
+        } else {
+            for (int i = 0; i < reserve.length; i++) {
+                if (reserve[i] > lost[i]) {
+                    answer++;
+                }
             }
         }
         System.out.println(answer);
-
         return answer;
     }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         int n = 5;
-        int[] lost = new int[]{2, 4};
-        int[] reverse = new int[]{3};
+        int[] lost = new int[]{4,5};
+        int[] reverse = new int[]{1,2,3};
         gymSuit sol = new gymSuit();
         sol.solution(n, lost, reverse);
 
