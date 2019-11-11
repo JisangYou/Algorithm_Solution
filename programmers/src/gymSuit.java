97c177bd309d3e1d0447333dd7906f6f74883343
youjisang <jisangman89@gmail.com>
2019-11-11 23:58:13 +0900

    gymSuit, greedy 문제 ~ing

---
+++ programmers/src/gymSuit.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class gymSuit {
+    public int solution(int n, int[] lost, int[] reserve) {
+
+        int answer = n - lost.length;
+
+        for (int i = 0; i < lost.length; i++) {
+            if (reserve[i] < lost[i]) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int n = 5;
+        int[] lost = new int[]{2, 4};
+        int[] reverse = new int[]{3};
+        gymSuit sol = new gymSuit();
+        sol.solution(n, lost, reverse);
+
+    }
+}
