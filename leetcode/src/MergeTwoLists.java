e5e618cf5444146bddd71e9a9b204de29b72e901
youjisang <jisangman89@gmail.com>
2019-12-11 22:59:23 +0900

    RemoveDuplicatesFromSortedArray

--- leetcode/src/MergeTwoLists.java
+++ leetcode/src/MergeTwoLists.java
@@ -8,8 +8,7 @@ public class MergeTwoLists {
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        // if(l1 == null) return l2;
-        // else if(l2 == null) return l1;
+
         ListNode head = new ListNode(0);
         ListNode tempNode = head;
         while (l1 != null && l2 != null) {
---
+++ leetcode/src/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,48 @@
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+
+        int index = 0;
+
+        Set set = new LinkedHashSet();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+
+        Iterator<Integer> iter = set.iterator();
+        while (iter.hasNext()) {
+            nums[index++] = iter.next();
+        }
+
+//        for (int i = 0; i < set.size(); i++) {
+//            System.out.println(nums[i]);
+//        }
+        return index;
+
+        /**
+         *
+         */
+
+//        if (nums.length == 0) return 0;
+//        int i = 0;
+//        for (int j = 1; j < nums.length; j++) {
+//            if (nums[j] != nums[i]) {
+//                i++;
+//                nums[i] = nums[j];
+//            }
+//        }
+//        return i + 1;
+
+    }
+
+    public static void main(String[] args) {
+        //TODO 추후에 다시 한번 확인 해볼 문제.
+
+        int[] input = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        RemoveDuplicatesFromSortedArray sol = new RemoveDuplicatesFromSortedArray();
+        sol.removeDuplicates(input);
+    }
+}
