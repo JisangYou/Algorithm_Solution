985bb327030568ed6646bd4f090a66f9b23f3df2
youjisang <jisangman89@gmail.com>
2020-03-17 00:04:33 +0900

    그래프 문제 풀이
    추후에 다시 확인이 필요하고, 그래프 관련해서 학습이 필요함.( 많은 시간을 들였지만 해결하지 못함.  너무 오래 잡고 있지말고 풀이를 보기.)

--- leetcode/src/FindTheTownJudge.java
+++ leetcode/src/FindTheTownJudge.java
@@ -1,62 +1,46 @@
-import javax.sound.midi.SysexMessage;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+
 
 public class FindTheTownJudge {
 
+//    The town judge trusts nobody.
+//    Everybody (except for the town judge) trusts the town judge.
+//    There is exactly one person that satisfies properties 1 and 2.
+
     public int findJudge(int N, int[][] trust) {
 
-        ArrayList arr1 = new ArrayList();
-        ArrayList arr2 = new ArrayList();
-        Set set1 = new HashSet();
+        int[] voted = new int[N + 1]; // 각 사람이 다른 사람에게 주는 총 투표
+        int[] votes = new int[N + 1];  // 각사람이 많은 총 투표
 
         for (int i = 0; i < trust.length; i++) {
-            arr1.add(trust[i][0]);
-            set1.add(trust[i][1]);
+            voted[trust[i][0]]++; // 한사람이 다른 사람에게 주는 표, 내가 선사하는 표
+            votes[trust[i][1]]++; // 한사람이 다른 사람으로부터 받은 표,  선사 받은 표
+//            System.out.println("trust[i][0] = " + trust[i][0]);
+//            System.out.println("trust[i][1] = " + trust[i][1]);
+//            System.out.println("voted[trust[i][0]]++ = " + voted[trust[i][0]]++);
+//            System.out.println("votes[trust[i][1]]++ = " + votes[trust[i][1]]++);
         }
 
-        for (int i = 0; i < arr1.size(); i++) {
-            if (set1.contains(arr1.get(i))) {
-                arr2.add(trust[i][0]);
-                set1.remove(arr1.get(i));
+        for (int i = 1; i <= N; i++) {
 
+            if (votes[i] == N - 1 && voted[i] == 0) {  // 모든 사람들은 판사를 믿는다, 판사는 아무도 믿지 않는다.
+                return i;
             }
         }
 
-        if (set1.isEmpty()) {
-
-            N = -1;
-
-        } else {
-            Iterator<Integer> it = set1.iterator();
-            while (it.hasNext()) {
-//            System.out.println("set2: " + it.next());
-                N = it.next();
-            }
-
-            for (int i = 0; i < arr1.size(); i++) {
-                
-            }
-
-        }
-
-
-        return N;
+        return -1;
     }
 
     public static void main(String[] args) {
 
 //        int trust[][] = {{1, 3}, {2, 3}};
-        int trust[][] = {{1, 2}, {2, 3}};
-        int N = 3;
-//        int trust[][] = {{1, 3}, {2, 3}, {3, 1}};
-//        int trust[][] = {{1, 3}, {1, 4}, {2, 3}, {2, 4}, {4, 3}};
+//        int trust[][] = {{1, 2}, {2, 3}};
 //        int N = 3;
+//        int trust[][] = {{1, 3}, {2, 3}, {3, 1}};
+        int trust[][] = {{1, 3}, {1, 4}, {2, 3}, {2, 4}, {4, 3}};
+        int N = 4;
 
         FindTheTownJudge sol = new FindTheTownJudge();
-        System.out.print(sol.findJudge(N, trust));
+        System.out.print("sol = " + sol.findJudge(N, trust));
 
     }
 }
