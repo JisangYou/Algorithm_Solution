e93ab331f2e975d6e6559383cb2b0108aeaeaf54
youjisang <jisangman89@gmail.com>
2020-04-03 23:45:28 +0900

    모의고사 문제 풀이중
    완전탐색 문제

---
+++ programmers/src/MockExam.java
@@ -0,0 +1,17 @@
+public class MockExam {
+
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+
+        int[] answer = new int[]{1, 2, 3, 4, 5};
+        MockExam sol = new MockExam();
+        sol.solution(answer);
+
+    }
+}
