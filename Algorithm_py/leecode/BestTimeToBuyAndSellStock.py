1d08b95605e8a4a57032b6ec2e6f776931c6a4e6
JisangYou <jisangman89@gmail.com>
2020-09-10 01:05:27 +0900

    # todoReview : leetcode
    -121

--- Algorithm_py/leecode/BestTimeToBuyAndSellStock.py
+++ Algorithm_py/leecode/BestTimeToBuyAndSellStock.py
@@ -7,26 +7,42 @@ class Solution(object):
         두 수 중 큰지/ 작은지만 알면된다.
         """
 
+        """
+        if not prices:
+            return 0
+        minNum = prices[0]
+        maxNum = 0
+        for i in range(0, len(prices)):
+            minNum = min(minNum, prices[i])
+            maxNum = max(maxNum, prices[i] - minNum)
+
+        return maxNum
+        """
+
         if not prices:
             return 0
 
         mn = prices[0]
         mx = 0
-        for i in range(1, len(prices)):
+
+        for i in range(0, len(prices)):
+            # print("i1", i)
             if mn > prices[i]:
+                # print("mn", mn)
+                # print("i2", i)
                 mn = prices[i]
-
-            else:
-                if mx < prices[i]:
-                    mx = prices[i]
-                    mn =
-
+            temp = prices[i] - mn
+            print("temp", temp)
+            if mx < temp:
+                mx = temp
+        return mx
 
 
 # prices = [7, 6, 4, 3, 1]
 # prices = [1, 2, 4]
-prices = [2, 4, 1]
-# prices = [7, 1, 5, 3, 6, 4]
+# prices = [2, 4, 1]
+prices = [7, 1, 5, 3, 6, 4]
+# prices = [2, 1, 2, 1, 0, 1, 2]
 # prices = []
 solution = Solution()
 print(solution.maxProfit(prices))
