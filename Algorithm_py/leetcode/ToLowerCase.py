5336a09ff65dfcec0019b4145b56fb7c741e8b69
JisangYou <jisangman89@gmail.com>
2020-09-16 19:50:58 +0900

    # add : leetcode
    - 217

---
+++ Algorithm_py/leetcode/ToLowerCase.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        return str.lower()
+
+
+str = "Hello"
+solution = Solution()
+print(solution.toLowerCase(str))
